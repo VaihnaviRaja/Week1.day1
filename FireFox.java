98814d6f175cb9617f4af42232b1f7e5ca29d683
VaihnaviRaja <113991779+VaihnaviRaja@users.noreply.github.com>
2022-09-21 20:33:34 +0530

    Add files via upload

---
+++ FireFox.java
@@ -0,0 +1,17 @@
+package Week1.day1;
+
+public class FireFox {
+	public void launchBrowser() {
+		System.out.println("Launched Successfully");
+		// TODO Auto-generated method stub
+	}
+	public void maximize() {
+		// TODO Auto-generated method stub
+		System.out.println("It is Maximized");
+	}
+	public static void main(String[] args) {
+		FireFox f1 = new FireFox();
+		f1.launchBrowser();
+		f1.maximize();
+	}
+}
---
+++ Safari.java
@@ -0,0 +1,21 @@
+package Week1.day1;
+
+public class Safari {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=100.2f;
+		String name = "Safari";
+		boolean visiblity= true;
+		int year=1992;
+		char logo='m';
+		System.out.println(version);
+		System.out.println(name);
+		System.out.println(visiblity);
+		System.out.println(year);
+		System.out.println(logo);
+		
+
+	}
+
+}
